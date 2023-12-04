8c7daa62ccc966c23ba66e80e40e3a10e83fd960
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-04 12:42:41 -0300

    Criando as rotas bases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'details', component: DetailsComponent },
+];
