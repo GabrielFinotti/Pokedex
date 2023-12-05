a904eb1f48642064f47483fb69ce7e8a31a4a4b7
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-12-05 02:21:31 -0300

    Update poke-list.component.ts

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -14,11 +14,13 @@ import { PokeApiService } from '../../services/poke-api.service';
   styleUrl: './poke-list.component.scss',
 })
 export class PokeListComponent implements OnInit {
+  // Variáveis do componente.
   private setAllPokemons: any;
   protected getAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) {}
 
+  // Recebendo os dados da api quando o componente é inicializado.
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(
       (res) => (
@@ -28,6 +30,7 @@ export class PokeListComponent implements OnInit {
     );
   }
 
+  // Mẃtodo executado para fazer a filtragem dos pokemons.
   protected getNamePoke(value: string) {
     const filter = this.setAllPokemons.filter((res: any) => {
       return !res.name.indexOf(value.toLocaleLowerCase());
